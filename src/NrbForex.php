9d044ecf92e0265ccb95bab420565d1244484379
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2015-02-22 19:28:57 -0600

    Initialized class file.

---
+++ src/NrbForex.php
@@ -0,0 +1,17 @@
+<?php
+namespace NrbForex;
+
+class NrbForex
+{
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function call()
+    {
+    }
+}
